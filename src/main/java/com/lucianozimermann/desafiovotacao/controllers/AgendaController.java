f98cef4f2ee93ce1d32654864f3a9da18e5b90e4
Luciano Zimermann <lucianozn16@gmail.com>
2025-08-16 09:45:21 -0300

    feat: added agenda register

--- src/main/java/com/lucianozimermann/desafiovotacao/controllers/AgendaController.java
+++ src/main/java/com/lucianozimermann/desafiovotacao/controllers/AgendaController.java
@@ -1,13 +1,12 @@
 package com.lucianozimermann.desafiovotacao.controllers;
 
+import com.lucianozimermann.desafiovotacao.dto.requests.AgendaDTO;
+import com.lucianozimermann.desafiovotacao.dto.responses.AgendaResponseDTO;
 import com.lucianozimermann.desafiovotacao.entities.Agenda;
 import com.lucianozimermann.desafiovotacao.services.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "agendas")
@@ -16,8 +15,13 @@ public class AgendaController {
     @Autowired
     private AgendaService service;
 
+    @PostMapping
+    public ResponseEntity<AgendaResponseDTO> register(@RequestBody AgendaDTO dto) {
+       return ResponseEntity.ok(service.register(dto));
+    }
+
     @GetMapping(value = "{id}")
     public ResponseEntity<Agenda> findyById(@PathVariable Long id) {
-        return ResponseEntity.ok( service.findById(id));
+        return ResponseEntity.ok(service.findById(id));
     }
 }
--- src/main/java/com/lucianozimermann/desafiovotacao/services/AgendaService.java
+++ src/main/java/com/lucianozimermann/desafiovotacao/services/AgendaService.java
@@ -1,5 +1,7 @@
 package com.lucianozimermann.desafiovotacao.services;
 
+import com.lucianozimermann.desafiovotacao.dto.requests.AgendaDTO;
+import com.lucianozimermann.desafiovotacao.dto.responses.AgendaResponseDTO;
 import com.lucianozimermann.desafiovotacao.entities.Agenda;
 import com.lucianozimermann.desafiovotacao.exceptions.AgendaNotFoundException;
 import com.lucianozimermann.desafiovotacao.repositories.AgendaRepository;
@@ -11,6 +13,21 @@ public class AgendaService {
 
     @Autowired
     private AgendaRepository repository;
+
+    public AgendaResponseDTO register( AgendaDTO dto) {
+        Agenda agenda = Agenda.builder()
+                              .name(dto.getName())
+                              .description(dto.getDescription())
+                              .build();
+
+        agenda = repository.save(agenda);
+
+        return AgendaResponseDTO.builder()
+                                .id(agenda.getId())
+                                .name(agenda.getName())
+                                .description(agenda.getDescription())
+                                .build();
+    }
 
     public Agenda findById(Long id) {
         Agenda agenda = repository.findById(id)
