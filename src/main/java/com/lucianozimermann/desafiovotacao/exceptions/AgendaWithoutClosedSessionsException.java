53cfc2b8e7a7f15256672627d005b4ad6abbd292
Luciano Zimermann <lucianozn16@gmail.com>
2025-08-17 20:04:30 -0300

    fix: adjusted agenda without closed sessions exception message

--- src/main/java/com/lucianozimermann/desafiovotacao/exceptions/AgendaWithoutClosedSessionsException.java
+++ src/main/java/com/lucianozimermann/desafiovotacao/exceptions/AgendaWithoutClosedSessionsException.java
@@ -2,6 +2,6 @@ package com.lucianozimermann.desafiovotacao.exceptions;
 
 public class AgendaWithoutClosedSessionsException extends RuleConflictException{
     public AgendaWithoutClosedSessionsException() {
-        super("Não existem Sessões de votação nesta Pauta!");
+        super("Não existem Sessões de Votação finalizadas nesta Pauta!");
     }
 }
