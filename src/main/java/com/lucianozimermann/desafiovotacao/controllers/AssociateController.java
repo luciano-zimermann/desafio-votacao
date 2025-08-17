6a39bdb11625242224f257e92078911cf3202007
Luciano Zimermann <lucianozn16@gmail.com>
2025-08-17 17:37:45 -0300

    feat: added associate implementations

--- src/main/java/com/lucianozimermann/desafiovotacao/controllers/AssociateController.java
+++ src/main/java/com/lucianozimermann/desafiovotacao/controllers/AssociateController.java
@@ -1,13 +1,13 @@
 package com.lucianozimermann.desafiovotacao.controllers;
 
-import com.lucianozimermann.desafiovotacao.entities.Associate;
+import com.lucianozimermann.desafiovotacao.dto.requests.AssociateDTO;
+import com.lucianozimermann.desafiovotacao.dto.responses.AssociateResponseDTO;
 import com.lucianozimermann.desafiovotacao.services.AssociateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "associates")
@@ -15,8 +15,20 @@ public class AssociateController {
     @Autowired
     private AssociateService service;
 
+    @PostMapping
+    public ResponseEntity<AssociateResponseDTO> registerAssociate(@RequestBody AssociateDTO dto) {
+        return ResponseEntity.ok(service.register(dto));
+    }
+
     @GetMapping(value = "{id}")
-    public ResponseEntity<Associate> findyById(@PathVariable Long id) {
+    public ResponseEntity<AssociateResponseDTO> findyById(@PathVariable Long id) {
         return ResponseEntity.ok(service.findById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AssociateResponseDTO>> getAllAssociates() {
+        List<AssociateResponseDTO> associates = service.getAllAssociates();
+
+        return ResponseEntity.ok(associates);
     }
 }
--- src/main/java/com/lucianozimermann/desafiovotacao/services/AssociateService.java
+++ src/main/java/com/lucianozimermann/desafiovotacao/services/AssociateService.java
@@ -1,21 +1,51 @@
 package com.lucianozimermann.desafiovotacao.services;
 
+import com.lucianozimermann.desafiovotacao.dto.requests.AssociateDTO;
+import com.lucianozimermann.desafiovotacao.dto.responses.AssociateResponseDTO;
 import com.lucianozimermann.desafiovotacao.entities.Associate;
 import com.lucianozimermann.desafiovotacao.exceptions.AssociateNotFoundException;
 import com.lucianozimermann.desafiovotacao.repositories.AssociateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AssociateService {
-
     @Autowired
     private AssociateRepository repository;
 
-    public Associate findById(Long id) {
+    public AssociateResponseDTO register(AssociateDTO dto) {
+        Associate associate = Associate.builder()
+                                       .name(dto.getName())
+                                       .cpf(dto.getCpf())
+                                       .build();
+
+        associate = repository.save(associate);
+
+        return buildAssociateResponseDTO(associate);
+    }
+
+    public AssociateResponseDTO findById(Long id) {
         Associate associate = repository.findById(id)
                                         .orElseThrow(AssociateNotFoundException::new);
 
-        return associate;
+        return buildAssociateResponseDTO(associate);
+    }
+
+    public List<AssociateResponseDTO> getAllAssociates() {
+        return repository.findAll()
+                         .stream()
+                         .map(a -> buildAssociateResponseDTO(a))
+                         .collect(Collectors.toList());
+    }
+
+    private AssociateResponseDTO buildAssociateResponseDTO(Associate associate) {
+        return AssociateResponseDTO.builder()
+                .id(associate.getId())
+                .name(associate.getName())
+                .cpf(associate.getCpf())
+                .build();
     }
 }
